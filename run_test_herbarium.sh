8afdd41760af6088a5df414c240a5b7605ed8153
John Deck <jdeck88@gmail.com>
2019-01-01 11:14:54 -0800

    remove explicity run_test_ commands

--- run_test_herbarium.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python ./process.py test_herbarium test_data/data/test_herbarium/output --input_dir test_data/data/test_herbarium/input --config_dir test_data/config --ontology https://raw.githubusercontent.com/PlantPhenoOntology/ppo/master/releases/2017-10-20/ppo.owl --base_dir test_data/projects/test_herbarium --project_base test_data.projects.test_herbarium --verbose
