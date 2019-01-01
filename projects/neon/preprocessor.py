befeaa820f306db24143bb9932d4b09ac8ed5a80
John Deck <jdeck88@gmail.com>
2019-01-01 11:06:52 -0800

    making pandas warning go away by adding empty column names

--- projects/neon/preprocessor.py
+++ projects/neon/preprocessor.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
+"""preprocessor.AbstractPreProcessor implementation for preprocessing neon data"""
 
-"""proprocessor.AbstractPreProcessor implementation for preprocessing neon data"""
-
+import os, csv, argparse
+from zipfile import ZipFile
 import logging
 import multiprocessing
 import pandas as pd
+import numpy as np
 
-from zipfile import ZipFile
 from preprocessor import AbstractPreProcessor
 
-from projects.neon.helpers import walk_files, INTENSITY_VALUE_FRAME
 
+INTENSITY_FILE = os.path.join(os.path.dirname(__file__), 'intensity_values.csv')
+INTENSITY_VALUE_FRAME = pd.read_csv(INTENSITY_FILE, skipinitialspace=True, header=0) if os.path.exists(
+    INTENSITY_FILE) else None
+INTENSITY = 0
+PHENO_DESC = 1
 COLUMNS_MAP = {
     'uid': 'record_id',
     'dayOfYear': 'day_of_year',
@@ -21,12 +26,11 @@ COLUMNS_MAP = {
     'decimalLongitude': 'longitude'
 }
 
-
 class PreProcessor(AbstractPreProcessor):
     def _process_data(self):
-        num_processes = multiprocessing.cpu_count()
-        with multiprocessing.Pool(processes=num_processes) as pool:
-            pool.map(self._process_zip, [f for f in walk_files(self.input_dir)])
+        # loop all of the files in the NEON directory
+        for f in walk_files(self.input_dir):
+            self._process_zip(f)
 
     def _process_zip(self, file):
         statusintensity_file = None
@@ -85,6 +89,12 @@ class PreProcessor(AbstractPreProcessor):
             lambda row: row['date'].timetuple().tm_yday if pd.isnull(row['dayOfYear']) and pd.notnull(row['date']) else
             row['dayOfYear'], axis=1)
 
+        data['sub_source'] = ''
+        data['upper_percent_partplant'] = ''
+        data['upper_percent_wholeplant'] = ''
+        data['lower_percent_partplant'] = ''
+        data['lower_percent_wholeplant'] = ''
+
         df = data.merge(INTENSITY_VALUE_FRAME, left_on='phenophaseIntensity', right_on='value', how='left')
 
         # check that we have a 'value' value if we have a phenophaseIntensity value
@@ -102,6 +112,90 @@ class PreProcessor(AbstractPreProcessor):
         df.loc[df.phenophaseIntensity.isnull() & df.phenophaseStatus.str.match('no', case=False), 'upper_count_partplant'] = 0
         df.loc[df.phenophaseIntensity.isnull() & df.phenophaseStatus.str.match('no', case=False), 'upper_count_wholeplant'] = 0
 
-        df.fillna('', inplace=True)  # replace all null values
 
-        return df.rename(columns=COLUMNS_MAP)
+        df = df.fillna('')  # replace all null values
+
+        df = df.rename(columns=COLUMNS_MAP)
+
+        return df
+
+
+def walk_files(input_dir):
+    for root, dirs, files in os.walk(input_dir):
+
+        for file in files:
+            if file.endswith(".zip"):
+                yield os.path.join(root, file)
+
+
+def generate_phen_descriptions(input_dir):
+    found_values = set()
+
+    for file in walk_files(input_dir):
+        csv_file = None
+
+        with ZipFile(file) as zip_file:
+
+            for filename in zip_file.namelist():
+                if '.phe_statusintensity.' in filename:
+                    if csv_file:
+                        raise RuntimeError('multiple csv files found in zip_file {}'.format(zip_file.filename))
+                    csv_file = zip_file.open(filename)
+
+        if not csv_file:
+            raise RuntimeError('didnt file csv file in zip_file {}'.format(zip_file.filename))
+
+        data = pd.read_csv(csv_file, header=0, chunksize=1000000, skipinitialspace=True,
+                           usecols=['phenophaseName'])
+
+        for chunk in data:
+            found_values.update(chunk.phenophaseName.unique().tolist())
+
+    with open(os.path.join(os.path.dirname(__file__), 'phenophase_descriptions.csv'), 'w') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(['field', 'defined_by'])
+
+        for value in found_values:
+            writer.writerow([value, ''])
+
+
+def generate_intensity_values(input_dir):
+    if INTENSITY_VALUE_FRAME is None:
+        intensity_frame = pd.DataFrame([], columns=['value', 'lower_count', 'upper_count', 'lower_percent',
+                                                    'upper_percent'])
+    else:
+        intensity_frame = INTENSITY_VALUE_FRAME
+
+    found_values = set()
+
+    for file in walk_files(input_dir):
+        csv_file = None
+
+        with ZipFile(file) as zip_file:
+
+            for filename in zip_file.namelist():
+                if '.phe_statusintensity.' in filename:
+                    if csv_file:
+                        raise RuntimeError('multiple csv files found in zip_file {}'.format(zip_file.filename))
+                    csv_file = zip_file.open(filename)
+
+        if not csv_file:
+            raise RuntimeError('didnt file csv file in zip_file {}'.format(zip_file))
+
+        data = pd.read_csv(csv_file, header=0, chunksize=1000000, skipinitialspace=True,
+                           usecols=['phenophaseIntensity'])
+
+        for chunk in data:
+            found_values.update(chunk.phenophaseIntensity.unique().tolist())
+
+    found_values.difference_update(intensity_frame.index.tolist())
+
+    intensity_frame = intensity_frame.append(
+        pd.DataFrame.from_items([('value', list(found_values))])
+    )
+
+    intensity_frame.drop_duplicates('value', inplace=True)
+    intensity_frame.set_index('value', inplace=True)
+
+    intensity_frame.to_csv(INTENSITY_FILE)
+
