b35c9e1984fd45c47ef2feb12523222620bff9ec
John Deck <jdeck88@gmail.com>
2019-08-05 08:51:34 -0700

    modifying run.sh to conform to new ontology-data-pipeline command line structure

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
-PROJECT=$1
-if [ -z $PROJECT ]
-   then
-     echo "Usage: run.sh {project}"
-     echo "Current projects are neon, pep725, npn, herbarium."
-     echo "This bash script runs the pipeline for each of these projects using data in test_data directory" 
-     echo "NOTE that this does not actually run the TEST script itself, but is used to run through the pipeline"
-     echo "on the test_data... useful for testing the tests"
-     exit 0
-fi
+#INPUT_DATAFILE=$1
+#OUTPUT_DIRECTORY=$2
+
+#if [[ -z $INPUT_DATAFILE ]] || [[ -z $OUTPUT_DIRECTORY ]]
+#   then
+#     echo "Usage: run.sh {INPUT_DATAFILE} {OUTPUT_DIRECTORY}"
+#     echo ""
+#     echo "This bash script runs the pipeline for any INPUT_DATAFILE and places output in the specified OUTPUT_DIRECTORY."
+#     exit 0
+#fi
+
+PROJECT=npn
 
 # check that we have the latest ...
 docker pull jdeck88/ontology-data-pipeline
@@ -15,9 +17,25 @@ docker pull jdeck88/ontology-data-pipeline
 docker run -t -v "$(pwd)":/process -w=/app -ti jdeck88/ontology-data-pipeline \
     python pipeline.py \
     -v --drop_invalid \
-    $PROJECT \
-    /process/data/$PROJECT/input \
-    /process/data/$PROJECT/output \
+    --project_base /process/projects \
+    --input_dir /process/data/$PROJECT/input/ \
+    --project $PROJECT \
+    /process/data/npn/output \
     https://raw.githubusercontent.com/PlantPhenoOntology/ppo/master/releases/2018-10-26/ppo.owl \
     /process/config \
-    /process/projects \
+
+#usage: pipeline.py [-h] [--project PROJECT] [--input_dir INPUT_DIR]
+#                   [--project_base PROJECT_BASE] [--data_file DATA_FILE]
+#                   [--preprocessor PREPROCESSOR] [--drop_invalid] [--log_file]
+#                   [--reasoner_config REASONER_CONFIG] [-v] [-c CHUNK_SIZE]
+#                   [--num_processes NUM_PROCESSES] [-s SPLIT_DATA_COLUMN]
+#                   output_dir ontology config_dir
+#pipeline.py: error: the following arguments are required: output_dir, ontology, config_dir
+
+#docker run -t -v "$(pwd)":/process -w=/app -ti jdeck88/ontology-data-pipeline \
+#    python pipeline.py \
+#    -v --drop_invalid \
+#    --data_file /process/$INPUT_DATAFILE \
+#    /process/$OUTPUT_DIRECTORY \
+#    https://raw.githubusercontent.com/PlantPhenoOntology/ppo/master/releases/2018-10-26/ppo.owl \
+#    /process/config \
