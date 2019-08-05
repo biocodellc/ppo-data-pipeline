b4550174e5a3ad7a86d8c5c5507420138dd7df94
John Deck <jdeck88@gmail.com>
2019-08-05 10:35:00 -0700

    renaming test_data.sh to sample_data.sh

--- sample_data.sh
+++ sample_data.sh
@@ -1,11 +1,10 @@
 PROJECT=$1
 if [ -z $PROJECT ]
    then
-     echo "Usage: test_data.sh {project}"
+     echo "Usage: sample_data.sh {project}"
      echo "Current projects are neon, pep725, npn, herbarium."
-     echo "This bash script runs the pipeline for each of these projects using data in test_data directory" 
-     echo "NOTE that this does not actually run the TEST script itself, but is used to run through the pipeline"
-     echo "on the test_data... useful for testing the tests"
+     echo "This script runs the pipeline for minimal examples of each of the projects."
+     echo "...NOT a substitute for real tests"
      exit 0
 fi
 
@@ -16,9 +15,10 @@ docker pull jdeck88/ontology-data-pipeline
 docker run -v "$(pwd)":/process -w=/app -ti jdeck88/ontology-data-pipeline \
     python pipeline.py \
     -v --drop_invalid \
-    $PROJECT \
-    /process/test_data/$PROJECT/input \
-    /process/test_data/$PROJECT/output \
+    --project $PROJECT \
+    --project_base /process/projects \
+    --input_dir /process/sample_data/$PROJECT/input/ \
+    /process/sample_data/$PROJECT/output \
     https://raw.githubusercontent.com/PlantPhenoOntology/ppo/master/releases/2018-10-26/ppo.owl \
     /process/config \
-    /process/projects \
+
