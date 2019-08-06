19cef372b3c0e22b2310c244cf211c8250adb4f8
John Deck <jdeck88@gmail.com>
2019-08-06 15:30:04 -0700

    creating a special npn runner for just processing npn data, outside of ontology-data-pipeline.  this is first attempt at stand-alone preprocessor

---
+++ projects/npn/npn_runner.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+import os, csv, shutil
+import multiprocessing
+import pandas as pd
+
+PROJECT = 'npn'
+
+ROOT_PATH = os.path.join(os.path.dirname(__file__), '../../')
+INPUT_DIR = os.path.join(ROOT_PATH,'data', PROJECT, 'input')
+OUTPUT_DIR = os.path.join(ROOT_PATH, 'data', PROJECT, 'output')
+OUTPUT_FILE = os.path.join(OUTPUT_DIR, 'data.csv')
+CONFIG = os.path.join(ROOT_PATH, 'config')
+
+PHENOPHASE_DESCRIPTIONS_FILE = os.path.join(os.path.dirname(__file__), 'phenophase_descriptions.csv')
+DATASET_METADATA_FILE = os.path.join(os.path.dirname(__file__), 'ancillary_dataset_data.csv')
+
+COLUMNS_MAP = {
+        'observation_id': 'record_id',
+        'species': 'specific_epithet',
+        'phenophase_description': 'phenophase_name',
+        'Source': 'sub_source'
+        }
+
+
+class PreProcessor():
+
+    def main(self):
+
+        self.descriptions = pd.read_csv(PHENOPHASE_DESCRIPTIONS_FILE, header=0, skipinitialspace=True, dtype='object')
+
+        self.dataset_metadata = pd.read_csv(DATASET_METADATA_FILE, header=0, skipinitialspace=True,
+                usecols=['Dataset_ID', 'Source'], dtype='object')
+
+        num_processes = multiprocessing.cpu_count()
+        print (num_processes)
+        chunk_size = 1000
+
+        data = pd.read_csv(
+                os.path.join(INPUT_DIR, "npn_observations_data.csv"), 
+                header=0, 
+                engine='python', 
+                dtype='object', 
+                chunksize = chunk_size)
+
+		# clean
+        self._clean()
+
+        for chunk in data:
+
+            chunks = [chunk.ix[chunk.index[i:i + chunk_size]] for i in
+                range(0, chunk.shape[0], chunk_size)]
+
+            with multiprocessing.Pool(processes=num_processes) as pool:
+                print("pooling....")
+                pool.map(self._transform_chunk, chunks) 
+                print("finished pooling....")
+         
+    def _transform_chunk(self, chunk):
+        writeHeaders = True
+        if os.path.isfile(OUTPUT_FILE):
+            writeHeaders = False
+
+        print("\tprocessing next {} records".format(len(chunk)))
+        self._transform_data(chunk).to_csv(OUTPUT_FILE, columns=self._parse_headers(), mode='a', header=writeHeaders, index=False)
+
+    def _transform_data(self, data):
+        # Add an index name
+        # data.index.name = 'record_id'
+
+        # drop all records where the user is unsure of what was coded (this is phenophase_status = -1)
+        data = data[data.phenophase_status != -1]
+
+        # Handle cases where we want to force a default intensity value 
+        # First, fill out the force_default_value column with False where there is no value
+        self.descriptions['force_default_value'] = self.descriptions['force_default_value'].fillna(False)
+        # Second, apply, row by row a filter that sets the intensity_value to -9999 when we want to force defaults
+        data = data.apply(lambda row: self._force_defaults(row), axis=1)
+
+        # map counts from intensity_value table to upper and lower count/percents
+        cols = ['value', 'lower_count', 'upper_count', 'lower_percent', 'upper_percent']
+        data = self._translate(os.path.join(os.path.dirname(__file__), 'intensity_values.csv'), cols, 'value', data,
+                'intensity_value')
+
+        # when intensity_value != -9999 set upper/lower counts 
+        data = data.apply(lambda row: self._set_defaults(row), axis=1)
+
+        # set the source
+        data['source'] = 'USA-NPN'
+        data['basis_of_record'] = 'HumanObservation'
+        data = data.merge(self.dataset_metadata, left_on='dataset_id', right_on='Dataset_ID', how='left')
+
+        # Normalize Date to just Year. we don't need to store actual date because we use only Year + DayOfYear
+        data['year'] = pd.DatetimeIndex(data['observation_date']).year
+
+        # Create ScientificName
+        data['scientific_name'] = data['genus'] + ' ' + data['species']
+
+        # drop duplicate ObservationIDs
+        data.drop_duplicates('observation_id', inplace=True)
+
+        # filling in remaining column names, even though there is no data
+        data['individualID'] = ''
+        data['lower_count_wholeplant'] = ''
+        data['upper_count_wholeplant'] = ''
+        data['lower_count_partplant'] = ''
+        data['upper_count_partplant'] = ''
+
+        data = data.rename(columns=COLUMNS_MAP)
+
+        return data
+
+    # Get true/false value for related force_default column in phenophase_descriptions and override the intensity_value
+    # with -9999.  Force defaults overrides intensity_value descriptions with default values for phenophases where
+    # user count data do not make sense for PPO purposes.
+    def _force_defaults(self, row):
+        #print (self.descriptions['field']['defined_by']).values[0]
+        try:
+            if self.descriptions[(self.descriptions['field'] == row['phenophase_description'])]['force_default_value'].values[0]:
+                row['intensity_value'] = '-9999'
+        except IndexError:
+            # thrown if missing phenophase_description in phenophase_descriptions.csv file
+            pass
+
+        return row
+
+    # For any column with a -9999 in the intensity_value column, insert default values from the phenophase_descriptions sheet
+    def _set_defaults(self, row):
+        if row.intensity_value != '-9999':
+            return row
+
+        try:
+            if int(row.phenophase_status) == 0:
+                row['lower_percent_partplant'] = float(self.descriptions[self.descriptions['field'] == row['phenophase_description']]['lower_percent_absent'].values[0])
+                row['upper_percent_partplant'] = float(self.descriptions[self.descriptions['field'] == row['phenophase_description']]['upper_percent_absent'].values[0])
+                row['lower_percent_wholeplant'] = float(self.descriptions[self.descriptions['field'] == row['phenophase_description']]['lower_percent_absent'].values[0])
+                row['upper_percent_wholeplant'] = float(self.descriptions[self.descriptions['field'] == row['phenophase_description']]['upper_percent_absent'].values[0])
+            else:
+                row['lower_percent_partplant'] = float(self.descriptions[self.descriptions['field'] == row['phenophase_description']]['lower_percent_present'].values[0])
+                row['upper_percent_partplant'] = float(self.descriptions[self.descriptions['field'] == row['phenophase_description']]['upper_percent_present'].values[0])
+                row['lower_percent_wholeplant'] = float(self.descriptions[self.descriptions['field'] == row['phenophase_description']]['lower_percent_present'].values[0])
+                row['upper_percent_wholeplant'] = float(self.descriptions[self.descriptions['field'] == row['phenophase_description']]['upper_percent_present'].values[0])
+        except IndexError:
+            # thrown if missing phenophase_description in phenophase_descriptions.csv file
+            pass
+
+        return row
+
+    @staticmethod
+    def _translate(filename, cols, index_name, data_frame, lookup_column):
+        """
+         Function to read in a CSV file containing one or more values that we want
+         to use to translate values  for.  using the dataframe's "lookup_column"
+         as the key
+        """
+        # loop all columns
+        for column in cols:
+            # don't look at index column
+            if column is not index_name:
+                # read the incoming lookup filename into a dictionary using the
+                # the appropriate columns to assign the dictionary key/value
+                with open(filename) as f:
+                    this_dict = dict((rows[cols.index(index_name)], rows[cols.index(column)]) \
+                            for rows in csv.reader(f))
+                    # assign the new column name values based on lookup column name
+                    data_frame[column] = data_frame[lookup_column].map(this_dict)
+        return data_frame
+
+    # read the mapping.csv file and convert to a list to use as column headers
+    # This replaces the need for a separate headers.csv file
+    def _parse_headers(self):
+        file = os.path.join(CONFIG, 'mapping.csv')
+        if os.path.exists(file):
+            with open(file) as f:
+                reader = csv.reader(f, skipinitialspace=True)
+                self.headers = next(reader)
+            df = pd.read_csv(file)
+            self.headers = df['column'].unique().tolist() 
+
+    def _clean(self):
+        if os.path.exists(OUTPUT_DIR):
+            shutil.rmtree(OUTPUT_DIR)
+        os.makedirs(OUTPUT_DIR)
+
+if __name__ == '__main__':
+    PreProcessor().main()
